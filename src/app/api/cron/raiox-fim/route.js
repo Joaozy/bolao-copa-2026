149b42f4fda50ea5e96d048230234b15ceda0bb3
Joao Francisco <joao.netinhu43@gmail.com>
2026-07-10 18:22:22 -0300

    melhoria do raiox 2

--- src/app/api/cron/raiox-fim/route.js
+++ src/app/api/cron/raiox-fim/route.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 export async function GET(request) {
   try {
